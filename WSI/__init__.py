8b77081a5449af90498655147923739d54e03e87
Zsíros Zsombor <zsiroszsombor02@gmail.com>
2024-04-20 17:43:04 +0200

    Implement segmentation using StarDist

--- WSI/__init__.py
+++
@@ -1,8 +0,0 @@
-from slides import (
-    list_slides,
-    save_slides_mpp_otsu,
-)
-
-from tiles import run_tiling
-
-from filters import calculate_filters
--- WSI/lmao.py
+++
@@ -1,4 +0,0 @@
-from slides import list_slides
-
-
-list_slides(r"C:\Users\Zsombi\Downloads\PKG - Biobank_CMB-CRC_v3\CMB-CRC")
